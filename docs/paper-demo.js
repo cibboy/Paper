535218f9af0c3b6a41f496b3335c4d6d3092fcf6
cibboy <cibboy@gmail.com>
2018-08-28 11:29:20 +0200

    Small correction to links style. Continuing work on demo menu.

--- docs/paper-demo.js
+++ docs/paper-demo.js
@@ -12,3 +12,52 @@ function toggleMenu() {
 
 	menuOpen = !menuOpen;
 }
+
+function changeTheme(theme) {
+	if (theme == "light")
+		document.getElementById("base-css").setAttribute('href', 'paper-light.css');
+	else if (theme == "dark")
+		document.getElementById("base-css").setAttribute('href', 'paper-dark.css');
+}
+
+function bodyLean(lean) {
+	if (lean == "left") {
+		document.getElementById('body').classList.remove('lean-right');
+		document.getElementById('body').classList.add('lean-left');
+	}
+	else if (lean == "right") {
+		document.getElementById('body').classList.remove('lean-left');
+		document.getElementById('body').classList.add('lean-right');
+	}
+	else if (lean == "center") {
+		document.getElementById('body').classList.remove('lean-right');
+		document.getElementById('body').classList.remove('lean-left');
+	}
+}
+
+function justify(justification) {
+	if (justification == "left") {
+		document.getElementById('body').classList.remove('right');
+		document.getElementById('body').classList.remove('center');
+		document.getElementById('body').classList.remove('justify');
+		document.getElementById('body').classList.add('left');
+	}
+	else if (justification == "right") {
+		document.getElementById('body').classList.remove('left');
+		document.getElementById('body').classList.remove('center');
+		document.getElementById('body').classList.remove('justify');
+		document.getElementById('body').classList.add('right');
+	}
+	else if (justification == "center") {
+		document.getElementById('body').classList.remove('right');
+		document.getElementById('body').classList.remove('left');
+		document.getElementById('body').classList.remove('justify');
+		document.getElementById('body').classList.add('center');
+	}
+	else if (justification == "justify") {
+		document.getElementById('body').classList.remove('right');
+		document.getElementById('body').classList.remove('center');
+		document.getElementById('body').classList.remove('left');
+		document.getElementById('body').classList.add('justify');
+	}
+}
